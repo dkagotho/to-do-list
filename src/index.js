bfca37893651adcf1a060563d16e7e2825ac6194
Diana kagotho <dianakago2@gmail.com>
2021-04-07 13:34:32 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+/**
+  As suggested in comment the above line was missing,
+  -- Thanks Ross.
+*/
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<APP />, document.getElementById("root"));
