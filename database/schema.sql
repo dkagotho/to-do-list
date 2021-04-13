e5ef89778b05c372c70fbfde5b6b8b41f533ee31
Diana kagotho <dianakago2@gmail.com>
2021-04-13 09:12:28 -0400

    changes table name

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,12 @@
 CREATE DATABASE to_do_list;
 USE to_do_list;
-CREATE TABLE to_do_list_items(
-id int not null auto_increment,
-completed varchar(150) NOT NULL,
+CREATE TABLE to_dos
+(
+    id int not null
+    auto_increment,
+completed varchar
+    (150) NOT NULL,
 uncompleted boolean,
-primary key (id)
+primary key
+    (id)
 );
