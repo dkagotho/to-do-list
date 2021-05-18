aac64d5800b749e52f4679ed226ab2bb1cd651cb
Diana kagotho <dianakago2@gmail.com>
2021-05-17 20:36:07 -0400

    added a filter for statuses

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,17 +2,29 @@ import React from "react";
 import Todo from "./Todo";
 import { useCollection } from "react-ketting";
 
-const TodoList = ({ todos, setTodos, filteredTodos }) => {
+const TodoList = ({ todos, setTodos, filteredTodos, status }) => {
   const { loading, error, items } = useCollection("/todo", {
     refreshOnStale: true,
   });
+
   if (loading) return <p>Loading...</p>;
   if (error) return <div className="error">{error.message}</div>;
 
+  // console.log(status);
+  var filteredItems = [...items];
+
+  if (status != "all") {
+    var completedStatus = true;
+    if (status == "uncompleted") {
+      completedStatus = false;
+    }
+    filteredItems = items.filter((data) => data.completed == completedStatus);
+  }
+  console.log(filteredItems);
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {items.map((todo) => {
+        {filteredItems.map((todo) => {
           console.log(todo.id);
           return <Todo key={todo.id} todo={todo} />;
         })}
